22f16d7a9ed935f0a571ec02c922668f25253b07
Cody McCarty <codyjmccarty@gmail.com>
2023-04-09 13:12:40 -0500

    updated control inputs

--- Tank.h
+++ Tank.h
@@ -19,12 +19,17 @@ class TOONTANKS_API ATank : public ABasePawn
 public:
 	ATank();
 
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 private:
 	UPROPERTY(VisibleAnywhere);
 	USpringArmComponent* SpringArm;
 
 	UPROPERTY(VisibleAnywhere);
 	UCameraComponent* Camera;
+
+	void Move(float Value);
 
 
 };
